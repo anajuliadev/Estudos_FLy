ae1d4b4dae21c8569d811b851507fe18b48fb5fb
AnaJulia <anaju.silvaalves@gmail.com>
2025-01-26 23:29:24 -0300

    botao reiniciar

--- Alura-fly/praticando com desafios/sorteador-numeros/app.js
+++ Alura-fly/praticando com desafios/sorteador-numeros/app.js
@@ -13,13 +13,14 @@ function sortear(){
 
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
-        }
+        };
 
         sorteados.push(numero);
-
     };
     
+    
     document.getElementById('exibirResultado').innerHTML = sorteados;
+    alterarStatusBotao();
   /*   let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`; */
 
@@ -35,4 +36,28 @@ function sortear(){
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function alterarStatusBotao() {
+
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else{
+        botao.classList.add('container__botao-desabilitado');
+        botao.classList.remove('container__botao');
+    };
+};
+
+function reiniciar () {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+
+    document.getElementById('exibirResultado').innerHTML = 'Nenhum até agora';
+    alterarStatusBotao();
 };
