2856c2c543a798e248d79651d3a6cfe18c9a40bb
Luis Fonseca <lf.caetanofonseca@outlook.com>
2025-02-03 21:53:59 -0300

    finalizando o projeto

--- Alura-fly/praticando com desafios/alugames/js/app.js
+++ Alura-fly/praticando com desafios/alugames/js/app.js
@@ -2,7 +2,16 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let img = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    if(img.classList.contains('dashboard__item__img--rented')) {
+
+        img.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        
+    } else{
+        img.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    };
 }
