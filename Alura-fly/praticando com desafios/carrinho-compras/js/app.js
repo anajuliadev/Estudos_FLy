48e8168db47895dc7c7623c1d13184e295688e42
Luis Fonseca <123905779+anajuliadev@users.noreply.github.com>
2025-02-09 14:53:39 -0300

    funcao total e limpar

--- Alura-fly/praticando com desafios/carrinho-compras/js/app.js
+++ Alura-fly/praticando com desafios/carrinho-compras/js/app.js
@@ -1,3 +1,9 @@
+let total;
+limpar();
+
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0'
+
 function adicionar() {
     //recuperar valores: nome e qntd
     let produto = document.getElementById('produto').value;
@@ -6,16 +12,25 @@ function adicionar() {
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
 
+
     //calc o subtotal por produto
     let preco = quantidade * valorUnitario;
     //adicionar ao carrinho
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section          class="carrinho__produtos__produto">
-        <span class="texto-azul">${quantidade}X</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
-        </section>`
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto"> <span class="texto-azul">${quantidade}X</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span> </section>`;
+
+
     //atualizar o total dos produtos
+    total = total + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${total}`;
+    document.getElementById('quantidade').value = 0;
 };
 
+
+ 
 function limpar() {
-    
+    total = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'
 }
